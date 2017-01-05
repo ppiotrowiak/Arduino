0f4e0873511a728d8a695fe8e45f5bf0da5c1aba
ppiotrowiak <p.piotrowiak@yahoo.ie>
2017-01-05 20:27:26 +0000

    add back serial

--- ReedSwitchInterruptOLEDMicroPro/ReedSwitchInterruptOLEDMicroPro.ino
+++ ReedSwitchInterruptOLEDMicroPro/ReedSwitchInterruptOLEDMicroPro.ino
@@ -4,6 +4,17 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+/*
+ * #define is a useful C component that allows the programmer to give a name to a constant value before the program is compiled. Defined constants in arduino don't take up any program memory space on the chip. 
+ * The compiler will replace references to these constants with the defined value at compile time.
+ */
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
 volatile word steps;//load the variable from RAM and not from a storage register
 volatile unsigned long speedTimes[2]; //load the variable from RAM and not from a storage register
 
@@ -16,10 +27,25 @@ unsigned long speedFactor = 0;
 volatile unsigned int speed = 0;
 void setup()
 {
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
+  // init done
   
   pinMode(1, INPUT_PULLUP); //interrupt pin 
   attachInterrupt(0, onStep, RISING);//dodaj przerwanie do pinu 1 (tx0) [ pin 1 is interrupt 3]
   speedFactor = circ*hour/distFact; //wstępne obliczenia
+
+  display.clearDisplay();
+  display.setCursor(10, 10);
+  display.setTextColor(WHITE);
+  display.setTextSize(3);
+  display.print("Cycluino");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  display.display();
 }
 
 void loop()
@@ -35,6 +61,16 @@ void loop()
     speed = 0;
   } 
 
+  for(int i = 0; i < sizeof(speedTimes)/sizeof(long); i++)
+  {
+    Serial.print(speedTimes[i]);
+    Serial.print(" ");
+  }
+  //Serial.println(sizeof(speedTimes)/sizeof(long));
+  Serial.print("Speed interval: ");
+  Serial.print(wheelRotationInterval);
+  Serial.print(" Speed: ");
+  Serial.println(speed);
   
   delay(1000);
 
