8c23fbf168f4aa8ecb50782580339d9f0a143015
ppiotrowiak <p.piotrowiak@yahoo.ie>
2017-03-19 22:21:46 +0000

    dodanie kodu do visualstudio

---
+++ DigitalReadSerial_encode/Debug/DigitalReadSerial_encode.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\Przemyslaw\AppData\Local\Temp\VMBuilds\DigitalReadSerial_encode\nano_atmega328\Debug\DigitalReadSerial_encode.cpp.o: \
+ C:\Users\Przemyslaw\AppData\Local\Temp\VMBuilds\DigitalReadSerial_encode\nano_atmega328\Debug\DigitalReadSerial_encode.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\uhhl2rqf.aow\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
--- DigitalReadSerial_encode/DigitalReadSerial_encode.ino
+++ DigitalReadSerial_encode/DigitalReadSerial_encode.ino
@@ -5,10 +5,41 @@
  This example code is in the public domain.
  */
 
+class Encoder
+{
+	// Class member variables
+	// These are initialized at startup
+	int _range; // ilosc "pozycji" enkodera
+	int _pinA; // pin A enkodera
+	int _pinB; // pin B enkodera
+	int _pinButton; // pin przycisku enkodera
+
+	// This properties maintain current state
+	int currentPosition; //aktualna "pozycja" enkodera
+	int previousPosition; //poprzednia "pozycja" enkodera
+	bool button; // stan przycisku
+	bool posChanged; // wskaznik zmiany pozycji
+
+	// Constructor - creates an encoder and intializes the members variable and state
+public: Encoder(int pinA, int pinB, int pinButton, int range)
+{
+	_range = range;
+	_pinA = pinA;
+	_pinB = pinB;
+	_pinButton = pinButton;
+
+	previousPosition = currentPosition = 0;
+
+	}
+};
+
 // digital pin 2 has a pushbutton attached to it. Give it a name:
 int pushButton = 2;
 int dt = 3;
 int clk = 4;
+
+int encoder_state; //to idzie do obiektu
+int licznik; //to tez idzie do obiektu
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -18,6 +49,9 @@ void setup() {
   pinMode(pushButton, INPUT);
   pinMode(dt, INPUT);
   pinMode(clk, INPUT);
+
+  licznik = 0;
+  encoder_state = 3; //stan podstawowy
 }
 
 // the loop routine runs over and over again forever:
@@ -26,12 +60,43 @@ void loop() {
   int buttonState = digitalRead(pushButton);
   int dtState = digitalRead(dt);
   int clkState = digitalRead(clk);
-  // print out the state of the button:
-  Serial.print(buttonState);
-  Serial.print(dtState);
-  Serial.println(clkState);
-  delay(1000);        // delay in between reads for stability
+
+  int encoder_state_temp = read_gray_code_from_encoder(dtState, clkState);
+  if (encoder_state != encoder_state_temp)
+  {
+    encoder_rotation(encoder_state, encoder_state_temp);
+    encoder_state = encoder_state_temp;
+  }
+    
+  delay(1);        // delay in between reads for stability
 }
 
+byte read_gray_code_from_encoder(int pin1, int pin2)
+{
+  byte val=0;
+  if (1 == pin1)
+    val += 2;
+  
+  if (1 == pin2)
+    val += 1;
+    
+  return val;
+}
+
+void encoder_rotation(int prev, int current)
+{
+  //if ((prev==3 && current==1) || (prev==0 && current==2))//clockwise
+  if (prev==3 && current==1)
+  {
+    licznik++;
+    Serial.println(licznik);
+  }
+  //else if ((prev==2 && current==0) || (prev==1 && current==3))
+  else if (prev==3 && current==2)
+  {
+    licznik--;
+    Serial.println(licznik);
+  }
+}
 
 
